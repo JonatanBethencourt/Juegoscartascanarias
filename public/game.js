f8870bf4eec04b3cd4b906ef048fc3065a164296
Jonatan Bethencourt <jonatanbethencourtabreu@gmail.com>
2026-08-23 15:22:46 -0400

    Añadidos servidores TURN gratuitos de Open Relay Project para solucionar bloqueos de videollamada entre países

--- public/game.js
+++ public/game.js
@@ -743,7 +743,17 @@ const peerConnections = {};
 const rtcConfig = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:stun1.l.google.com:19302' }
+    { urls: 'stun:stun1.l.google.com:19302' },
+    {
+      urls: [
+        'stun:openrelay.metered.ca:80',
+        'turn:openrelay.metered.ca:80',
+        'turn:openrelay.metered.ca:443',
+        'turn:openrelay.metered.ca:443?transport=tcp'
+      ],
+      username: 'openrelayproject',
+      password: 'openrelayproject'
+    }
   ]
 };
 
